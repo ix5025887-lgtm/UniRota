f4fbc5b6e018bf03c2867ca220794d51ee6bce1f
ix5025887-lgtm <ix5025887@gmail.com>
2025-10-29 18:24:27 -0300

    caminho adm
    Added loading messages and improved user feedback during login.

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ while True:
             posicao_senha = linhas.index(email)+1
             if linhas[posicao_senha] == senha:
                 posicao_tipo = linhas.index(email)-1
-                print(f"Entrando como {linhas[posicao_tipo]}")
-                break
+                sleep(0.5)
+                print(f"Entrando como {linhas[posicao_tipo]}...")
+                sleep(1)
+                screen.clear()
             else:
                 print("Email ou senha incorretos...")
                 sleep(2)
@@ -41,11 +43,13 @@ while True:
 
     if opc == 3:
         break
-if linhas[posicao_tipo] == "administrador":
-    print("Adm entrou")
+while posicao_tipo != "":
+    route.caminho_adm(linhas, posicao_tipo)
+    if linhas[posicao_tipo] == "administrador":
+        GeralDef.caminho_adm(linhas, posicao_tipo)
 
-if linhas[posicao_tipo] == "aluno":
-    print("Aluno entrou")
+    if linhas[posicao_tipo] == "aluno":
+        print("Aluno entrou")
 
-if linhas[posicao_tipo] == "motorista":
-    print("motorista entrou")
+    if linhas[posicao_tipo] == "motorista":
+        print("motorista entrou")
